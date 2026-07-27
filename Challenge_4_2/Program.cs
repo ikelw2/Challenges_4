d62245842937f3c1e6694d1ca4575f75ca7719dc
Michael Wood <ikelw2@gmail.com>
2026-07-27 23:58:47 +0200

    Add project files.

---
+++ Challenge_4_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Challenge 4_1 - determine if positive integer contains digit 3");
+
+
+// 
+// 2.Divisible by 2 or 3
+// 
+// Given two integers, write a method that returns their multiplication
+// if they are both divisible by 2 or 3, otherwise returns their sum.
+// 
+// Expected input and output
+// 
+// DivisibleBy2Or3(15, 30) → 450
+// DivisibleBy2Or3(2, 90) → 180
+// DivisibleBy2Or3(7, 12) → 19
+// 
+
+while (true)
+{
+
+
+
+
+    Random random = new Random();
+    int upperPowerOfTen = random.Next(1, 10);
+    int highLimit = (int)Math.Pow(10, upperPowerOfTen);
+    int randomPosNumber1 = random.Next(0, highLimit);
+    int randomPosNumber2 = random.Next(0, highLimit);
+
+    Console.WriteLine($"DivisibleBy2Or3({15}, {30}) --> {DivisibleBy2Or3(15, 30)}");
+    Console.WriteLine($"DivisibleBy2Or3({2}, {90}) --> {DivisibleBy2Or3(2, 90)}");
+    Console.WriteLine($"DivisibleBy2Or3({7}, {12}) --> {DivisibleBy2Or3(7, 12)}");
+    Console.WriteLine($"Random Test Numbers:\nDivisibleBy2Or3({randomPosNumber1}, {randomPosNumber2}) --> {DivisibleBy2Or3(randomPosNumber1, randomPosNumber2)}");
+
+
+
+
+    Console.Write("\nEnter Q to quit or press ENTER to continue.");
+    bool userWantsToQuit = (Console.ReadLine().Trim().Equals("q", StringComparison.OrdinalIgnoreCase) == true); // if user enters 'q' or 'Q'
+    if (userWantsToQuit == true)
+    {
+        break; // exit from main program loop
+    }
+    Console.WriteLine("-----------------------------------------------------");
+}
+Console.WriteLine();
+
+
+
+int DivisibleBy2Or3(int number1, int number2)  
+{
+    // if divisible by 2 and 3
+    if ((number1 % 2 == 0) || (number1 % 3 == 0))
+    {
+        if ((number2 % 2 == 0) || (number2 % 3 == 0))
+        {
+            return number1 * number2;
+        }
+    }
+    return number1 + number2;
+}
---
+++ Challenges_4_1/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("Challenge 4_1 - determine if positive integer contains digit 3");
+
+
+//
+// 1.If number contains 3
+// 
+// Write a method that checks if given number (positive integer) contains
+// digit 3. Do not convert number to other type. Do not use built-in
+// functions like Contains(), StartsWith(), etc.
+// 
+// Expected input and output
+// 
+// IfNumberContains3(7201432) → true IfNumberContains3(87501) → false
+//
+
+
+while (true)
+{
+
+
+
+
+    Random random = new Random();
+    int upperPowerOfTen = random.Next(1, 10);
+    int highLimit = (int)Math.Pow(10, upperPowerOfTen);
+    int randomPosNumber = random.Next(0, highLimit);
+
+    Console.WriteLine($"IfNumberContains3({randomPosNumber}) --> {IfNumberContains3(randomPosNumber)}");
+
+
+
+
+
+
+
+    Console.Write("\nEnter Q to quit or press ENTER to continue.");
+    bool userWantsToQuit = (Console.ReadLine().Trim().Equals("q", StringComparison.OrdinalIgnoreCase) == true); // if user enters 'q' or 'Q'
+    if (userWantsToQuit == true)
+    {
+        break; // exit from main program loop
+    }
+    Console.WriteLine("-----------------------------------------------------");
+}
+Console.WriteLine();
+
+
+
+bool IfNumberContains3(int number)  // had a more complex version of this, but used some 
+    // details from the volunteer in class to make this more better...
+{
+    //Console.Write($"TESTING '{number}'     ");
+    while (number > 0)
+    {
+        //Console.Write($"remainder = {number % 10}   ");
+        //Console.Write($"quotient = {number / 10}    \n");
+
+        if (number % 10 == 3)
+            return true;
+
+        number = number / 10;
+    }
+    return false;
+}
